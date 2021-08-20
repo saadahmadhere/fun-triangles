eee7513b159ed564147a89477b374155103651e2
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-08-21 00:09:31 +0530

    done quiz

--- src/TakeAQuiz.js
+++ src/TakeAQuiz.js
@@ -2,27 +2,43 @@ import { useState } from "react";
 
 const TakeAQuiz = () => {
     const correctAnswers = ["80°", "right angled" ]
+    const userAnswers = [];
 
-    let score = 0
+    var score = 0
     const [userAnswer1, setUserAnswer1] = useState(null)
     const [userAnswer2, setUserAnswer2] = useState(null)
+    const [scoreDisplay, setScoreDisplay] = useState(null)
 
     const submitButtonHandler = (e) =>{
         e.preventDefault();
+        
+        userAnswers.push(userAnswer1, userAnswer2);
+       
+        for(let i=0; i<correctAnswers.length; i++){
+            if (correctAnswers[i] === userAnswers[i])
+                score = score +1
+        }
 
-        if(userAnswer1 === "80°")
-            score +=1
-        else
+        setScoreDisplay(score)
+        console.log("ans1", userAnswer1);
+        console.log("correct answers: ", correctAnswers);
+        console.log("user answers: ", userAnswers);
+        console.log("score: ", score);
     }
+
+    
 
     return (
         <div className="quiz blur">
+            <h1>Quiz on triangles</h1>
+            <h2>For each correct answer you will get 1 point</h2>
             <form onSubmit={submitButtonHandler} className= "quizForm">
                 <div className="questionContainer">
 
                     <h3>What is the third angle for a triangle if it's first angle is 45° and the second angle is 55°?</h3>
                     <label>
-                        <input type="radio" name= "question-1" value="45°" onChange = {e=>{setUserAnswer1(e.target.value)}}/>
+                        <input type="radio" name= "question-1" value="45°" onChange = {e=>{ console.log(e)
+                        setUserAnswer1(e.target.value)}}/>
                         45°
                     </label>
                     <label>
@@ -55,11 +71,9 @@ const TakeAQuiz = () => {
                     </label>
                     
                 </div>
-                <button type="submit" >Check the Answers</button>
+                {scoreDisplay || scoreDisplay === 0 ? <h2>Your score is {scoreDisplay}</h2> : <button type="submit" >Check the Answers</button>}
+                
             </form>
-
-            <div> ans1: {userAnswer1}</div>
-            <div> ans2: {userAnswer2}</div>
 
         </div>
      );
