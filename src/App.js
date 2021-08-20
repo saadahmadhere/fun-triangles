bbdf865dba0fa18f975776bb783068e8b3c6570e
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-08-21 00:19:10 +0530

    bring footer in app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,46 @@ import AnglesOfTriangle from './AnglesOfTriangle';
 import CheckHypotenuse from './CheckHypotenuse'
 import CalculateArea from './CalculateArea'
 import TakeAQuiz from './TakeAQuiz'
-import {Footer} from './Footer'
+// import {Footer} from './Footer'
 import Home from './Home'
+
+
+export const Footer = () => {
+  return ( 
+      <footer>
+      <ul>
+      <li className="footerLink">
+          <a href="mailto:saadahmad728@gmail.com">
+            <i className="fa fa-envelope" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li className="footerLink">
+          <a href="https://github.com/saadahmadhere/">
+            <i className="fab fa-github" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li className="footerLink">
+          <a href="https://twitter.com/imsaadahmad">
+            <i className="fab fa-twitter"></i>
+          </a>
+        </li>
+        <li className="footerLink">
+          <a href="https://www.linkedin.com/in/saadahmadhere/">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+        </li>
+        <li className="footerLink">
+          <a href="https://saadahmad.netlify.app/">
+            <i className="fas fa-briefcase"></i>
+          </a>
+        </li>
+
+      </ul>
+    </footer>
+   );
+}
+
+
 
 function App() {
   return (
